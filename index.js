4483e258c93d26402765774d7d011bc01435fc3d
Matheus Vinícius Valenza <mvalenza@fesctecnologia.com.br>
2020-01-16 13:06:22 -0300

    cleanning up comments

--- index.js
+++ index.js
@@ -3,27 +3,20 @@ var app = express()
 var bodyParser = require('body-parser')
 const axios = require('axios')
 
-app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
-) // for parsing application/x-www-form-urlencoded
+)
 
-//This is the route the API will call
 app.post('/new-message', function(req, res) {
   const { message } = req.body
 
-  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-
   if (!message || message.text.toLowerCase().indexOf('matheus') < 0) {
-    // In case a message is not present, or if our message does not have the word matheus in it, do nothing and return an empty response
     return res.end()
   }
 
-  // If we've gotten this far, it means that we have received a message containing the word "matheus".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
   axios
     .post(
       `https://api.telegram.org/bot${process.env.API_TOKEN}/sendMessage`,
@@ -33,18 +26,15 @@ app.post('/new-message', function(req, res) {
       }
     )
     .then(response => {
-      // We get here if the message was successfully posted
       console.log('Message posted')
       res.end('ok')
     })
     .catch(err => {
-      // ...and here if it was not
       console.log('Error :', err)
       res.end('Error :' + err)
     })
 })
 
-// Finally, start our server
 app.listen(process.env.PORT || 3000, function() {
   console.log('Telegram app listening on port !', process.env.PORT || 3000)
 })
